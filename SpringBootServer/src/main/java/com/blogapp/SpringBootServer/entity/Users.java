114fea9f9f63aa58c0d4c446cff1897302de5654
m.laic <m.laic@hotmail.com>
2024-05-02 21:41:26 +0200

    added @Entity for class Users

---
+++ SpringBootServer/src/main/java/com/blogapp/SpringBootServer/entity/Users.java
@@ -0,0 +1,72 @@
+package com.blogapp.SpringBootServer.entity;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Users {
+    private int id;
+    private String name;
+    private String lastname;
+    private String email;
+    private String password;
+
+    private String isFavorite;
+    private String comment;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getIsFavorite() {
+        return isFavorite;
+    }
+
+    public void setIsFavorite(String isFavorite) {
+        this.isFavorite = isFavorite;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
